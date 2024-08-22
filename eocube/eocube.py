a4c5241ead30536f83e1efa6a99ddc1d13aa0cb2
Luana Becker da Luz <luanabeckerdaluz@gmail.com>
2024-08-22 15:31:00 -0300

    Update eocube.py

--- eocube/eocube.py
+++ eocube/eocube.py
@@ -187,12 +187,14 @@ class DataCube:
 
 
     def _initialize_stac_client(self):
+        print("Initializing STAC...")
         # parameters = dict(access_token=config.ACCESS_TOKEN)
         response = requests.get(config.STAC_URL, verify=False)
         if response.status_code == 200:
             return pystac_client.Client.from_dict(response.json())
         else:
             print("STAC_URL request didn't return code 200!")
+            
 
     def _validate_bbox(self, bbox):
         check_bbox_format(bbox, msg="Invalid bounding box parameter!")
